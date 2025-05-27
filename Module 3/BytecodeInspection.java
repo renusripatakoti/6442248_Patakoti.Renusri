3eb5e486223ae5c241aadc58f2d38241ea05e801
renusripatakoti <renusripatakoti@gmail.com>
2025-05-27 17:36:22 +0530

    Delete Module 3 directory

--- Module 3/BytecodeInspection.java
+++
@@ -1,9 +0,0 @@
-public class BytecodeInspection {
-    public void sayHello() {
-        System.out.println("Hello bytecode!");
-    }
-
-    public static void main(String[] args) {
-        new BytecodeInspection().sayHello();
-    }
-}
--- Module 3/java-ModulesDemo/com.utils/utlis.java
+++
@@ -1,7 +0,0 @@
-package com.utils;
-
-public class Utils {
-    public static String getGreeting(String name) {
-        return "Hello, " + name + "! Welcome to Java Modules.";
-    }
-}
