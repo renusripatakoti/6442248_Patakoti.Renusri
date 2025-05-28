c5ad0db4ac8cc55930c451441515447ad03d521b
renusripatakoti <renusripatakoti@gmail.com>
2025-05-28 21:18:02 +0530

    Update MultiplicationTable.java

--- Module 3/Core_Java_Exercises/MultiplicationTable.java
+++ Module 3/Core_Java_Exercises/MultiplicationTable.java
@@ -1,15 +1,12 @@
-// MultiplicationTable.java
 import java.util.Scanner;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user to enter a number
         System.out.print("Enter a number to print its multiplication table: ");
         int number = scanner.nextInt();
 
-        // Use a for loop to print the table
         System.out.println("Multiplication table for " + number + ":");
         for (int i = 1; i <= 10; i++) {
             System.out.println(number + " x " + i + " = " + (number * i));
