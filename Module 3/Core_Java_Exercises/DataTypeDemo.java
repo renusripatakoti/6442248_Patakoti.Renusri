25fff13dba22ecb90fe427646b56a2e660a632e7
renusripatakoti <renusripatakoti@gmail.com>
2025-05-28 22:29:41 +0530

    Update DataTypeDemo.java

--- Module 3/Core_Java_Exercises/DataTypeDemo.java
+++ Module 3/Core_Java_Exercises/DataTypeDemo.java
@@ -1,14 +1,11 @@
-// DataTypeDemo.java
 public class DataTypeDemo {
     public static void main(String[] args) {
-        // Declare variables of different primitive types
         int myInt = 100;
-        float myFloat = 12.34f;       // Note the 'f' for float literal
+        float myFloat = 12.34f;       
         double myDouble = 56.789;
         char myChar = 'A';
         boolean myBoolean = true;
-
-        // Display the values
+        
         System.out.println("Integer value: " + myInt);
         System.out.println("Float value: " + myFloat);
         System.out.println("Double value: " + myDouble);
