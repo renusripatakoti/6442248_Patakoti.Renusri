5060f952bb60a073d5a58faa4715eaca946790f2
renusripatakoti <renusripatakoti@gmail.com>
2025-05-28 22:47:26 +0530

    Update TypeCastingExample.java

--- Module 3/Core_Java_Exercises/TypeCastingExample.java
+++ Module 3/Core_Java_Exercises/TypeCastingExample.java
@@ -1,16 +1,15 @@
-// TypeCastingExample.java
 public class TypeCastingExample {
     public static void main(String[] args) {
-        double myDouble = 9.78;
-        int myInt = (int) myDouble;  // Explicit casting: double to int
+        double d = 9.78;
+        int i = (int) d;  
 
-        System.out.println("Original double value: " + myDouble);
-        System.out.println("After casting to int: " + myInt);
+        System.out.println("Original double value: " + d);
+        System.out.println("After casting to int: " + i);
 
-        int myInt2 = 42;
-        double myDouble2 = myInt2;   // Implicit casting: int to double
+        int i2 = 42;
+        double d2 = i2;   
 
-        System.out.println("Original int value: " + myInt2);
-        System.out.println("After casting to double: " + myDouble2);
+        System.out.println("Original int value: " + i2);
+        System.out.println("After casting to double: " + d2);
     }
 }
