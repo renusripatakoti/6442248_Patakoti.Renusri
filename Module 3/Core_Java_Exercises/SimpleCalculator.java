3d303ef299e723d37f159744adf4c69bb07964db
renusripatakoti <renusripatakoti@gmail.com>
2025-05-28 21:00:27 +0530

    Update SimpleCalculator.java

--- Module 3/Core_Java_Exercises/SimpleCalculator.java
+++ Module 3/Core_Java_Exercises/SimpleCalculator.java
@@ -4,20 +4,17 @@ public class SimpleCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt user to enter two numbers
+        
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
 
         System.out.print("Enter second number: ");
         double num2 = scanner.nextDouble();
 
-        // Prompt user to choose an operation
         System.out.println("Choose an operation: +, -, *, /");
         char operator = scanner.next().charAt(0);
 
         double result;
-
-        // Perform the operation
         switch (operator) {
             case '+':
                 result = num1 + num2;
