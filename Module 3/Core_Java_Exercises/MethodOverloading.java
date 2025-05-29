c9c2565be36fdf6cd33a8e727824e2a6a9bec083
renusripatakoti <renusripatakoti@gmail.com>
2025-05-29 20:05:40 +0530

    Update MethodOverloading.java

--- Module 3/Core_Java_Exercises/MethodOverloading.java
+++ Module 3/Core_Java_Exercises/MethodOverloading.java
@@ -1,16 +1,13 @@
 public class MethodOverloading {
 
-    // Method to add two integers
     public static int add(int a, int b) {
         return a + b;
     }
 
-    // Method to add two doubles
     public static double add(double a, double b) {
         return a + b;
     }
 
-    // Method to add three integers
     public static int add(int a, int b, int c) {
         return a + b + c;
     }
