4b52541e5698ac9b9ba0c1c0d1e7687e6c345e3c
renusripatakoti <renusripatakoti@gmail.com>
2025-06-01 20:37:18 +0530

    Update InheritanceDemo.java

--- Module 3/Core_Java_Exercises/InheritanceDemo.java
+++ Module 3/Core_Java_Exercises/InheritanceDemo.java
@@ -1,11 +1,9 @@
-// Base class
 class Animal {
     void makeSound() {
         System.out.println("Some generic animal sound");
     }
 }
 
-// Subclass
 class Dog extends Animal {
     @Override
     void makeSound() {
@@ -15,12 +13,10 @@ class Dog extends Animal {
 
 public class InheritanceDemo {
     public static void main(String[] args) {
-        // Instantiate Animal class
         Animal genericAnimal = new Animal();
-        genericAnimal.makeSound(); // Output: Some generic animal sound
+        genericAnimal.makeSound(); 
 
-        // Instantiate Dog class
         Dog dog = new Dog();
-        dog.makeSound(); // Output: Bark
+        dog.makeSound(); 
     }
 }
