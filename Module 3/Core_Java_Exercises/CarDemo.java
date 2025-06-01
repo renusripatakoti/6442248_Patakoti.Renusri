7987bb85cf43619670c6809e3dbc3755c6cf0263
renusripatakoti <renusripatakoti@gmail.com>
2025-06-01 20:08:31 +0530

    Update CarDemo.java

--- Module 3/Core_Java_Exercises/CarDemo.java
+++ Module 3/Core_Java_Exercises/CarDemo.java
@@ -1,10 +1,8 @@
 class Car {
-    // Attributes
     String make;
     String model;
     int year;
 
-    // Method to display car details
     void displayDetails() {
         System.out.println("Car Make: " + make);
         System.out.println("Car Model: " + model);
@@ -15,19 +13,16 @@ class Car {
 
 public class CarDemo {
     public static void main(String[] args) {
-        // Create first Car object
         Car car1 = new Car();
         car1.make = "Toyota";
         car1.model = "Camry";
         car1.year = 2022;
 
-        // Create second Car object
         Car car2 = new Car();
         car2.make = "Tesla";
         car2.model = "Model 3";
         car2.year = 2023;
 
-        // Call method to display details
         System.out.println("Car 1 Details:");
         car1.displayDetails();
 
