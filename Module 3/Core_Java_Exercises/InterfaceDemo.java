76769827c5eba3cae07c76dbabdc4a034f469c4e
renusripatakoti <renusripatakoti@gmail.com>
2025-06-01 22:00:47 +0530

    Update InterfaceDemo.java

--- Module 3/Core_Java_Exercises/InterfaceDemo.java
+++ Module 3/Core_Java_Exercises/InterfaceDemo.java
@@ -1,29 +1,25 @@
-// Define the interface
 interface Playable {
-    void play(); // abstract method
+    void play(); 
 }
 
-// Implementing the interface in Guitar class
 class Guitar implements Playable {
     public void play() {
         System.out.println("Strumming the guitar...");
     }
 }
 
-// Implementing the interface in Piano class
 class Piano implements Playable {
     public void play() {
         System.out.println("Playing the piano keys...");
     }
 }
-
-// Main class to test
+;
 public class InterfaceDemo {
     public static void main(String[] args) {
         Playable guitar = new Guitar();
         Playable piano = new Piano();
 
-        guitar.play(); // Output: Strumming the guitar...
-        piano.play();  // Output: Playing the piano keys...
+        guitar.play(); 
+        piano.play();  
     }
 }
